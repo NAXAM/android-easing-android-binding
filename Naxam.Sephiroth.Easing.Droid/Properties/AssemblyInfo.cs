ca3b952021e8cd20ce8744808f313a9ed9a0dd38
Vu Duc Tuyen <tuyen@naxam.net>
2017-04-27 10:32:59 +0700

    Android Easing 1.0.2

--- Naxam.Sephiroth.Easing.Droid/Properties/AssemblyInfo.cs
+++ Naxam.Sephiroth.Easing.Droid/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Naxam.Sephiroth.Easing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Naxam.Sephiroth.Easing.Droid")]
+[assembly: AssemblyDescription("Xamarin Android Binding Library fro Android Easing")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Naxam.Sephiroth.Easing")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("NAXAM CO., LTD")]
+[assembly: AssemblyProduct("Xam Bindings")]
+[assembly: AssemblyCopyright("Copyright ©  2017 NAXAM")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
@@ -26,5 +26,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
---
+++ README.md
@@ -0,0 +1,6 @@
+# Xamarin Android Binding Library
+Xamarin Android Binding Library for [Sephiroth Android Easing](https://mvnrepository.com/artifact/it.sephiroth.android.library.easing/library)
+
+```
+Install-Package Naxam.Sephiroth.Easing.Droid
+```
